28ed051281f2de2950c67657709b3a34fdd0f2b2
fatmabenali1 <124519501+fatmabenali1@users.noreply.github.com>
2024-09-05 14:36:44 +0200

    Spring Boot project

---
+++ src/main/java/org/example/softrestbackends/SoftRestBackendSApplication.java
@@ -0,0 +1,13 @@
+package org.example.softrestbackends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftRestBackendSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftRestBackendSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/softrestbackends/SoftRestBackendSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.softrestbackends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftRestBackendSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
